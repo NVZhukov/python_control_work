c056a166e7f11a97b5279241592bc00f9875bd45
Zhukov Nikita <nypno@mail.ru>
2023-04-04 21:29:38 +0300

    changed save and add method

--- model/Model.py
+++ model/Model.py
@@ -1,32 +1,55 @@
 from datetime import datetime as dt
 from time import time
 import csv
+import os
 
-note_id = 1
+path = "notes.csv"
 
 
-def add_note():
-    global note_id
-    time = dt.now().strftime('%H:%M:%S')
-    header = input('Введите заголовок заметки: ')
-    note_body = input('Введите тело заметки: ')
-    note = [time, note_id, header, note_body]
-    note_id += 1
-    return note
+def add_note(path_to_file):
+    try:
+        with open(path_to_file, encoding='utf-8', mode='r+') as pb:
+            pb.seek(0, os.SEEK_END)
+            isempty = pb.tell()
+            if (isempty == 0):
+                note_id = 1
+                time = dt.now().strftime('%H:%M:%S')
+                header = input('Введите заголовок заметки: ')
+                note_body = input('Введите тело заметки: ')
+                note = [note_id, time, header, note_body]
+                return note
+            else:
+                with open(path_to_file, encoding='utf-8', mode='r+') as pb:
+                    for line in pb:
+                        pass
+                    note_id = int(line[1]) + 1
+                    time = dt.now().strftime('%H:%M:%S')
+                    header = input('Введите заголовок заметки: ')
+                    note_body = input('Введите тело заметки: ')
+                    note = [note_id, time, header, note_body]
+                    return note
+    except FileNotFoundError:
+        print('Укажите верный файл или путь к нему')
 
 
 def save(data):
     try:
-        with open('notes.csv', 'a', encoding="utf-8") as file:
-            file_writer = csv.writer(
-                file, delimiter=";", lineterminator="\r")
-            file_writer.writerow(data)
+        with open("notes.csv", 'a', encoding="utf-8") as file:
+            file.write('{}\n'.format(data))
     except FileNotFoundError:
-        with open('notes.csv', 'w', encoding="utf-8") as file:
-            file_writer = csv.writer(
-                file, delimiter=";", lineterminator="\r")
-            file_writer.writerow(data)
+        with open("notes.csv", 'w', encoding="utf-8") as file:
+            file.write('{}\n'.format(data))
 
 
-res = add_note()
+def load(path_to_file):
+    try:
+        with open(path_to_file, encoding='utf-8', mode='r+') as pb:
+            lines = pb.readlines()
+            print(''.join(lines))
+    except FileNotFoundError:
+        print('Укажите верный файл или путь к нему')
+
+
+res = add_note(path)
 save(res)
+# load(path)
